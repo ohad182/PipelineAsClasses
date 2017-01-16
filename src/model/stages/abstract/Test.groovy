bf8ab8bfe768495336dcb8947602ce3c95d75335
ohad182 <ohad182@gmail.com>
2017-01-16 09:38:17 +0200

    Update Test.groovy

--- src/model/stages/abstract/Test.groovy
+++ src/model/stages/abstract/Test.groovy
@@ -1,1 +1,12 @@
-asdsad
+package model.stages.abstract
+
+class StageInfo implements Serializable {
+
+    def stageName
+    def script
+
+    StageInfo(script, String stageName) {
+        this.script = script
+        this.stageName = stageName
+    }
+}
